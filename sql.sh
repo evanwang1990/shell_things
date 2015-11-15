f8354bb1af2c649bf7cd83f141aaf9dad52433b7
evanwang1990 <evanwang1990@gmail.com>
2015-11-16 00:18:46 +0800

    small update

--- sql.sh
+++ sql.sh
@@ -25,12 +25,17 @@ awk 'BEGIN{
 		rno[count]=NR
 		} 
 	END{
-		for (i=0;i<count;i++){
+		for (i=0;i<count;i++)
+		{
 			printf("sed -n '\''%s,%sp'\'' %s > %s_%s.sql\n",rno[i]+1,rno[i+1],"'${tmp_sql}'","'${tmp_sql%%.*}'",i)
-			}
+		}
 		}' ${tmp_sql} | bash  #flag 引号内引用外变量
 
 #execute each sql
 sqls=$(ls ${tmp_sql%%.*}_*)
 echo ${sqls[@]}
+for sql_str in ${sqls[@]}; do
+	echo "The sql script:"
+	cat ${sql_str}
+done
 
